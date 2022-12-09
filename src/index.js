24255b9f1b208b5134c5671876984f0c0b3a744c
leaf <dev0tea0leaf@gmail.com>
2022-12-09 13:49:11 +0200

    config

--- src/index.js
+++ src/index.js
@@ -7,23 +7,7 @@ import App from "./App";
 import { Web3ReactProvider } from "@web3-react/core";
 import Web3 from "web3";
 import { Web3Modal } from "@web3modal/react";
-import { chains, providers } from "@web3modal/ethereum";
-import { binanceSmartChain } from "./web3/connector";
-
-const config = {
-  projectId: process.env.PROJECT_ID,
-  theme: "dark",
-  accentColor: "orange",
-  ethereum: {
-    appName: "web3Modal",
-    chains: [binanceSmartChain],
-    providers: [
-      providers.walletConnectProvider({
-        projectId: process.env.PROJECT_ID,
-      }),
-    ],
-  },
-};
+import { CONFIG } from "./web3/constants";
 
 const getLibrary = (provider) => {
   return new Web3(provider);
@@ -35,7 +19,7 @@ root.render(
     <React.StrictMode>
       <BrowserRouter>
         <App />
-        <Web3Modal config={config} />
+        <Web3Modal config={CONFIG} />
       </BrowserRouter>
     </React.StrictMode>
   </Web3ReactProvider>
--- src/web3/constants.js
+++ src/web3/constants.js
@@ -1,3 +1,20 @@
+import { providers } from "@web3modal/ethereum";
+import { binanceSmartChain } from "./connector";
+
 export const LOTTERY = "0x422D9AE3D58AAf94038791f796B5E1096Fd77905";
 export const TOKEN = "0x3b5328d38a795514e044081fcaa764013715c666";
 export const GAS = 360000;
+export const CONFIG = {
+  projectId: process.env.PROJECT_ID,
+  theme: "dark",
+  accentColor: "orange",
+  ethereum: {
+    appName: "web3Modal",
+    chains: [binanceSmartChain],
+    providers: [
+      providers.walletConnectProvider({
+        projectId: process.env.PROJECT_ID,
+      }),
+    ],
+  },
+};
